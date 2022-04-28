c5640c520086ecc006b57e66224c490780ca1568
mariacorbu <mariacorbu99@protonmail.com>
2022-04-28 19:22:24 +0300

    Initial code

---
+++ Teleasis-website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Teleasis_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
